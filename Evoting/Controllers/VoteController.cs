fde2085f79e5489e5817ef2a91d867b81a41065d
your name <nhoangtien95@gmail.com>
2017-11-29 18:54:52 +0700

    Index + SignUp + SignIn GUI

---
+++ Evoting/Controllers/VoteController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Evoting.Controllers
+{
+    public class VoteController : Controller
+    {
+        // GET: Vote
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ Evoting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Evoting.MvcApplication" Language="C#" %>
